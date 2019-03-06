8cdbc1ce9a51931f7a2b6bab5801d23a7d3143a7
Amalia Rodriguez <you@example.com>
2019-03-06 12:42:59 +0100

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,8 @@ if (newName) {
     alert("You forgot to enter your name");
 }
 
-
+document.getElementById("mesa").addEventListener("click", function () {
+    alert ("SOBRE LA MESA\nLorem ipsum dolor sit amet, consectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\nquis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in,\nreprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum")
+});
 
 }
