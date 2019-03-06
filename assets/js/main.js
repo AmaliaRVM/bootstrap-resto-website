cfe4473ccf06bd59eba343a1e0e38d1fdf5e3887
Amalia Rodriguez <you@example.com>
2019-03-06 11:52:55 +0100

    add js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 window.onload = () => {
-var name = prompt("Hello, can I have your name please");
-document.getElementById("popUp").innerText = ("Welcome " + name + " !");
+let name = prompt("Hello, can I have your name please");
+let newName = name
+if (newName) {
+    document.getElementById("popUp").innerText = ("Welcome " + newName + " !");
+} else {
+    alert("You forgot to enter your name");
+}
+
 
 
 }
